2729c0bbea145dc0b6d21f1cfc10dd659ddf17b3
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-01 01:48:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -41,9 +41,3 @@ class Artist
     @songs.each {|song| puts song.name}
   end
 end
-
-artist = Artist.new('Michael Jackson')
-song_one = Song.new("Rock With You")
-song_two = Song.new("Smooth Criminal")
-artist.add_song(song_one)
-artist.add_song(song_two)
