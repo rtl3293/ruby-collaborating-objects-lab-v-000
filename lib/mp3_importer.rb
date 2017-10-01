267407008ef4cddbeabe5e5955f6b01753b4c3b7
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-01 02:15:29 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,7 +11,7 @@ class MP3Importer
   def files
     array_of_files = Dir["#{@path}/**/*.mp3"]
     normalized_files = array_of_files.collect do |file|
-      file.basename
+      file.split("{@path}")
     end
   end
 end
